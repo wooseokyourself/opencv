c5b47f2b41c1a425445986bb1671d3c6632d7c3c
WOOSEOK CHOI <21500740@handong.edu>
2020-05-01 17:26:06 +0900

    Update elementwise_layers.cpp

--- modules/dnn/src/layers/elementwise_layers.cpp
+++ modules/dnn/src/layers/elementwise_layers.cpp
@@ -759,12 +759,15 @@ struct SwishFunctor : public BaseFunctor
 struct MishFunctor : public BaseFunctor
 {
     typedef MishLayer Layer;
-
+    float MISH_THRESHOLD;
+    
     bool supportBackend(int backendId, int)
     {
-        return backendId == DNN_BACKEND_OPENCV ||
-               backendId == DNN_BACKEND_CUDA ||
-               backendId == DNN_BACKEND_HALIDE || backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH;
+#ifdef HAVE_DNN_NGRAPH
+        if (backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH)
+            return INF_ENGINE_VER_MAJOR_GT(INF_ENGINE_RELEASE_2020_2);
+#endif
+        return backendId == DNN_BACKEND_OPENCV || backendId == DNN_BACKEND_HALIDE;
     }
 
     void apply(const float* srcptr, float* dstptr, int len, size_t planeSize, int cn0, int cn1) const
